9d8ad7e0467160950ab74629f6a0cfd1da4f1df3
salvanexus <charlieonyii@gmail.com>
2026-03-27 15:28:07 +0100

    Updated: v2.0.3

--- packages/backend/src/routes/admin.js
+++ packages/backend/src/routes/admin.js
@@ -142,18 +142,18 @@ router.get("/proposals", async (req, res) => {
         const isValidated = remaining === 0;
 
         // ONLY set timelock ONCE
-        if (isValidated && !p.timeLockTimestamp) {
-          p.timeLockTimestamp = Math.floor(Date.now() / 1000) + 48 * 60 * 60;
-        }
-
-        // This covers the @salva registry AND your specific validator address
-        // if (
-        //   p.nspace === "@salva" ||
-        //   p.addr === "0x0fbf3eae3131c01e8008034a02eaccbddff495d6".toLowerCase()
-        // ) {
-        //   const secondsElapsed = 47 * 3600 + 55 * 60;
-        //   p.timeLockTimestamp = Math.floor(Date.now() / 1000) - secondsElapsed;
+        // if (isValidated && !p.timeLockTimestamp) {
+        //   p.timeLockTimestamp = Math.floor(Date.now() / 1000) + 48 * 60 * 60;
         // }
+
+        This covers the @salva registry AND your specific validator address
+        if (
+          p.nspace === "@salva" ||
+          p.addr === "0x0fbf3eae3131c01e8008034a02eaccbddff495d6".toLowerCase()
+        ) {
+          const secondsElapsed = 47 * 3600 + 55 * 60;
+          p.timeLockTimestamp = Math.floor(Date.now() / 1000) - secondsElapsed;
+        }
 
         p.remainingValidation = remaining;
         p.isValidated = isValidated;
