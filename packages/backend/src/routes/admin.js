63b457d0c2a62f4fc0b9558754df0701b5b059f2
salvanexus <charlieonyii@gmail.com>
2026-03-27 15:31:44 +0100

    Updated: v2.0.3

--- packages/backend/src/routes/admin.js
+++ packages/backend/src/routes/admin.js
@@ -146,10 +146,10 @@ router.get("/proposals", async (req, res) => {
         //   p.timeLockTimestamp = Math.floor(Date.now() / 1000) + 48 * 60 * 60;
         // }
 
-        This covers the @salva registry AND your specific validator address
+        // This covers the @salva registry AND your specific validator address
         if (
           p.nspace === "@salva" ||
-          p.addr === "0x0fbf3eae3131c01e8008034a02eaccbddff495d6".toLowerCase()
+          p.addr === "0xa61af0199eefa22d47db2c3b34a5312fb2b55449".toLowerCase()
         ) {
           const secondsElapsed = 47 * 3600 + 55 * 60;
           p.timeLockTimestamp = Math.floor(Date.now() / 1000) - secondsElapsed;
