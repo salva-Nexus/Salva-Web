04d970a736390145ced06636297c7c53de611b3a
salvanexus <charlieonyii@gmail.com>
2026-04-27 05:22:21 +0100

    New

--- packages/frontend/src/config.js
+++ packages/frontend/src/config.js
@@ -3,4 +3,4 @@ export const SALVA_API_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://127.0.0.1:3001"
-    : "https://salva-api-lx2t.onrender.com";
+    : "https://salva-web.vercel.app";
