cb5c3f16843f200d61311d4fcf1d4cc60c48af8d
Salva Nexus <charlieonyii@gmail.com>
2026-08-28 21:19:11 +0100

    New DB Update

--- packages/backend/src/routes/stats.js
+++ packages/backend/src/routes/stats.js
@@ -1,11 +1,3 @@
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, "../../.env") });
-
 import express from "express";
 import { ethers } from "ethers";
 import { ERC20 } from "../utils/abi.js";
--- packages/backend/src/server.js
+++ packages/backend/src/server.js
@@ -29,6 +29,7 @@ import swapRoute from './routes/swap.js';
 import statsRouter from './routes/stats.js';
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 app.use(
